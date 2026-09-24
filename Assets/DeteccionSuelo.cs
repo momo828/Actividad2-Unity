99950a918f3e048daddcd48b44fc3b1d4f0a788e
Cynthia Ramirez <cynthiaramirez@MacBook-Air-de-Cynthia.local>
2026-09-24 14:51:37 -0600

    implementacion de mecanim

--- Assets/DeteccionSuelo.cs
+++ Assets/DeteccionSuelo.cs
@@ -2,20 +2,22 @@ using UnityEngine;
 
 public class DeteccionSuelo : MonoBehaviour
 {
-    // Esta variable guarda el estado de nuestro personaje
-    public bool estaEnElSuelo = false;
+    private Animator animador; // Variable para controlar tu Mecanim
 
-    // Esta función de Unity se dispara automáticamente cuando dos Colliders chocan
+    private void Start()
+    {
+        // Al iniciar, Unity busca tu Animator y lo guarda para usarlo
+        animador = GetComponent<Animator>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Comparamos si el objeto contra el que chocamos tiene la etiqueta "Suelo"
+        // Revisamos si con lo que chocamos tiene la etiqueta "Suelo"
         if (collision.gameObject.CompareTag("Suelo"))
         {
-            // Cambiamos el estado del objeto
-            estaEnElSuelo = true;
-            
-            // Enviamos un mensaje a la consola para demostrar que funciona
-            Debug.Log("¡Colisión detectada! El personaje ahora está en el suelo.");
+            // Encendemos el interruptor de tu Animator
+            animador.SetBool("enElSuelo", true); 
+            Debug.Log("¡Chocamos! Cambiando a estado de Reposo.");
         }
     }
 }
